4ef1c83973944bafe67ac84c9d1b17f781c252ec
Suganya <suganprakash0621@gmail.com>
2020-10-07 19:15:58 -0500

    Added images to fishdataprovider.ja

--- scripts/fishdataprovider.js
+++ scripts/fishdataprovider.js
@@ -2,6 +2,7 @@ const fishCollection = [
     
     {
         Name:"Clown Fish",
+        Image:"https://s4.thingpic.com/images/95/3V3RY19FsatfjyLWRDYjxgZH.jpeg",
         Species:"Actinopterygii",
         length:"7-8cm",
         Diet: "feed on small zooplankton from the water column, such as copepods and tunicate larvae,algae",
@@ -9,6 +10,7 @@ const fishCollection = [
        },
        {
         Name:"Jack",
+        Image:"https://i0.wp.com/oceanbites.org/wp-content/uploads/2016/02/clown-fish-300x225.jpg?resize=300%2C225",
         Species:"Carangidae",
         length:"100 cm",
         Diet: "prawns, shrimps, crabs, molluscs and stomatopods.",
@@ -16,6 +18,7 @@ const fishCollection = [
        },
        {
         Name:"Captain",
+        Image:"https://scx2.b-cdn.net/gfx/news/hires/2014/fishfromacid.jpg"
         Species:"Centropomidae",
         length:"70-80cm",
         Diet: "molluscs and stomatopods.",
@@ -23,6 +26,7 @@ const fishCollection = [
        },
        {
         Name:"Molly Fish",
+        Image:"https://oceana.org/sites/default/files/shutterstock_430721545_1.jpg"
         Species:"Pomacanthus angelfish",
         length:"50-80 cm",
         Diet: "feed on small zooplankton from the water column, such as copepods and tunicate larvae,algae",
