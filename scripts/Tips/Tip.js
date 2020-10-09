aa52224f760603afa98dae6453b48e55a111caf8
Suganya <suganprakash0621@gmail.com>
2020-10-09 10:44:06 -0500

    modifies some code

--- scripts/Tips/Tip.js
+++ scripts/Tips/Tip.js
@@ -3,10 +3,10 @@ export const Tips = (TipsObj) => {
     <div class="tips">
     <h3 class="tips__water">${TipsObj.Topic}</h3>
     <ol>
-        <p> class="tips__water" ${TipsObj.text}</p>
-        <p>class="tips__water" ${TipsObj.text}</p>
-        <p>class="tips__water" ${TipsObj.text}</p>
-        <p>class="tips__water" ${TipsObj.text}</p>
+        <p class="tips__water"> ${TipsObj.text}</p>
+        <p class="tips__water"> ${TipsObj.text}</p>
+        <p class="tips__water" >${TipsObj.text}</p>
+        <p class="tips__water"> ${TipsObj.text}</p>
     </ol>
     <div class="tips__fish"> 
        <h3 class="tips__fish"> ${TipsObj.Topic}</h3>
